54ca7351603f35fbdfa18c25789207eb1979bd18
IgorSorta <igorsorta@gmail.com>
2020-08-26 16:58:37 +0300

    Update knex file.

--- database/knex.js
+++ database/knex.js
@@ -1,9 +1,5 @@
-module.exports = require('knex')({
-    client: 'mysql2',
-    connection: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'igortestDB',
-        database: 'bug_tracker'
-    }
-});
+const knex = require('knex');
+
+const config = require('../knexfile');
+
+module.exports = knex(config.development);
