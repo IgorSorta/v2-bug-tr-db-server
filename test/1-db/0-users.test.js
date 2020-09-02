418d6860c0655f2324f4c62c0ead960e60f7627f
IgorSorta <igorsorta@gmail.com>
2020-09-02 17:06:36 +0300

    Users first test file was updated.

--- test/1-db/0-users.test.js
+++ test/1-db/0-users.test.js
@@ -6,29 +6,53 @@ const {
 describe('Testing a database.', () => {
 
 
-    describe('Request users from table.', () => {
+    describe('Request all users from table.', () => {
         let requestUsers = requestDbApi('http://127.0.0.1:3030/user/all');
 
         test('Should pass if request is correct.', async () => {
             const result = await requestUsers();
 
-            //console.log(result.data);
             expect(result).not.toBeNull();
+            expect(result.status).toBe(200);
+            expect(result.headers['content-type']).toBe('application/json; charset=utf-8')
+            expect(result.data.data).toHaveLength(3);
         });
     });
 
-    describe('Request user from table by id.', () => {
+    describe('Request user from table by ...', () => {
         let requestUser = requestDbApi('http://127.0.0.1:3030/user/params');
 
-        test('Should pass if request is correct.', async () => {
-            const params = {
-                user_id: 1
+        test(`Should pass if request {SELECT x FROM table WHERE param = value} is correct.`, async () => {
+            const data = {
+                select: 'email',
+                where: {
+                    name: 'Jojo'
+                }
             };
 
-            const result = await requestUser(params, 'POST');
+            const result = await requestUser(data, 'POST');
 
-            //console.log(result.data);
             expect(result).not.toBeNull();
+            expect(result.status).toBe(200);
+            expect(result.headers['content-type']).toBe('application/json; charset=utf-8')
+            expect(result.data.data).toHaveLength(1);
+        });
+
+        test('Should pass if request {SELECT [x1,x2,...] FROM table WHERE param1=value1 AND param2=value2 AND ...} is correct.', async () => {
+            const data = {
+                select: ["email", "name", "role"],
+                where: {
+                    name: 'Jojo',
+                    password: "jojo"
+                }
+            };
+
+            const result = await requestUser(data, 'POST');
+
+            expect(result).not.toBeNull();
+            expect(result.status).toBe(200);
+            expect(result.headers['content-type']).toBe('application/json; charset=utf-8')
+            expect(result.data.data).toHaveLength(1);
         });
     });
 
@@ -36,7 +60,18 @@ describe('Testing a database.', () => {
         let newUser = requestDbApi('http://127.0.0.1:3030/user/new');
 
         test('Should pass if user created successfully.', async () => {
+            const data = {
+                name: 'new user',
+                email: 'new@mail.com',
+                password: 'newnew',
+                role: 'USER'
+            };
+            const result = await newUser(data, 'POST');
 
+            expect(result).not.toBeNull();
+            expect(result.status).toBe(200);
+            expect(result.headers['content-type']).toBe('application/json; charset=utf-8')
+            expect(result.data.data).toHaveLength(1);
         });
     });
 
@@ -44,15 +79,44 @@ describe('Testing a database.', () => {
         let updateUser = requestDbApi('http://127.0.0.1:3030/user/update');
 
         test('Should pass if update request is correct.', async () => {
+            const data = {
+                param: {
+                    name: 'new user'
+                },
+                data: {
+                    email: 'updated@mail.com'
+                }
+            };
 
+            const result = await updateUser(data, 'POST');
+
+            expect(result).not.toBeNull();
+            expect(result.status).toBe(200);
+            expect(result.headers['content-type']).toBe('application/json; charset=utf-8')
+            expect(result.data).toEqual({
+                "data": 1,
+                "success": true
+            });
         });
     });
 
     describe('Delete a user.', () => {
-        let newUser = requestDbApi('http://127.0.0.1:3030/user/delete');
+        let deleteUser = requestDbApi('http://127.0.0.1:3030/user/delete');
 
         test('Should pass if delete operation is successful.', async () => {
+            const data = {
+                name: 'new user'
+            };
 
+            const result = await deleteUser(data, 'POST');
+
+            expect(result).not.toBeNull();
+            expect(result.status).toBe(200);
+            expect(result.headers['content-type']).toBe('application/json; charset=utf-8')
+            expect(result.data).toEqual({
+                "data": 1,
+                "success": true
+            });
         });
     });
 });
